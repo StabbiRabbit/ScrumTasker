3dcda1e738e2fca62ac961325106fc8fb2871005
JH51 <jonhuang5@gmail.com>
2022-11-30 12:37:18 -0500

    Refactor API Routing and front-end fetch requests (#3)
    * Added signup functionality on form submit
    
    * Renamed state variables for readability; renamed initial and invalid login messages
    
    * Renamed Effect hook inner function
    
    * Renamed Effect hook inner function; fixed recursive fetching on boards state change
    
    * Reintroduced 404 error handler; redirects to login page
    
    * Create new board feature adds a new board to database and lists new board in Dashboard; live render
    
    * /create/board endpoint returns only the created board instead of the updated list of boards; less traffic and faster rendering
    
    * Dashboard styling; button border and transitions
    
    * Partial migration of frontend of API path; linting
    
    * Dashboard styling
    
    * Moved signup POST to api router
    
    * Moved login GET, POST to api router
    
    * Moved dashboard, board GET, POST, DELETE to api router
    
    Co-authored-by: Rob Mosher <rob@robmosher.com>

--- server/express.js
+++ server/express.js
@@ -1,178 +1,45 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const path = require("path");
 const userController = require("./controllers/userController");
-const boardsController = require("./controllers/boardsController");
 const cookieController = require("./controllers/cookieController");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const { BACKEND_PORT } = process.env;
+
+app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:8080",
     credentials: true,
   })
 );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieParser());
-
-const PORT = 3000;
+const apiRouter = require("./routes/api");
+app.use("/api", apiRouter);
 
 app.get("/", (req, res) => {
   return res.status(202).sendFile(path.join(__dirname, "../build/index.html"));
 });
 
-app.post(
-  "/signup",
-  userController.validateUsername,
-  userController.createUser,
-  (req, res) => {
-    if (res.locals.createdUser === true) {
-      res.status(200).json({
-        username: res.locals.username,
-        boards: [],
-      });
-    } else {
-      res.status(501).send("Error Signing up");
-    }
-  }
-);
+// app.get("/login", (req, res) => {
+//   return res.status(200).sendFile(path.join(__dirname, "../build/bundle.html"));
+// });
 
-app.get(
-  "/dashboard",
-  cookieController.validateSSID,
-  cookieController.blockInvalidSession,
-  userController.getAllBoardsFromUser,
-  (req, res) => {
-    if (res.locals.ssidIsValid) res.status(200).json(res.locals.boardInfo);
-    else res.status(501).sendStatus("Invalid SSID");
-  }
-);
-
-app.get("/login", 
-  cookieController.validateSSID, (req, res) => {
-  if (res.locals.ssidIsValid) res.status(200).json(res.locals.boardInfo);
-  else res.status(501).sendStatus("Invalid SSID");
-});
-// app.post('')
-
-app.post(
-  "/login",
-  userController.validateUsername,
-  userController.validatePassword,
-  cookieController.setSSIDCookie,
-  (req, res) => {
-    if (res.locals.passwordIsValid && res.locals.usernameIsValid) {
-      res.status(200).json({
-        username: res.locals.username,
-        boards: res.locals.boards,
-      });
-    } else {
-      res.status(501).send("Error Logging In, Wrong Username or Password");
-    }
-  }
-);
-
-app.get(
-  "/board/:id",
-  cookieController.validateSSID,
-  cookieController.blockInvalidSession,
-  boardsController.getBoardFromUser,
-  (req, res) => {
-    return res.status(200).json(res.locals.boardInfo);
-  }
-);
-
-app.post(
-  "/create/board",
-  cookieController.validateSSID,
-  cookieController.blockInvalidSession,
-  boardsController.createBoard,
-  userController.getAllBoardsFromUser,
-  (req, res) => {
-    return res.status(200).json(res.locals.boardInfo);
-  }
-);
-
-app.post(
-  "/create/story",
-  cookieController.validateSSID,
-  cookieController.blockInvalidSession,
-  boardsController.createStory,
-  boardsController.getBoardFromUserUsingCache,
-  (req, res) => {
-    return res.status(200).json(res.locals.boardInfo);
-  }
-);
-
-app.post(
-  "/create/task",
-  cookieController.validateSSID,
-  cookieController.blockInvalidSession,
-  boardsController.createTask,
-  boardsController.getBoardFromUserUsingCache,
-  (req, res) => {
-    return res.status(200).json(res.locals.boardInfo);
-  }
-);
-
-app.delete(
-  "/delete/board",
-  cookieController.validateSSID,
-  cookieController.blockInvalidSession,
-  boardsController.deleteBoard,
-  userController.getAllBoardsFromUser,
-  (req, res) => {
-    return res.status(200).json(res.locals.boardInfo);
-  }
-);
-
-app.post(
-  "/delete/story",
-  cookieController.validateSSID,
-  cookieController.blockInvalidSession,
-  boardsController.createStory,
-  boardsController.getBoardFromUserUsingCache,
-  (req, res) => {
-    return res.status(200).json(res.locals.boardInfo);
-  }
-);
-
-app.post(
-  "/delete/task",
-  cookieController.validateSSID,
-  cookieController.blockInvalidSession,
-  boardsController.createTask,
-  boardsController.getBoardFromUserUsingCache,
-  (req, res) => {
-    return res.status(200).json(res.locals.boardInfo);
-  }
-);
-
-app.get("/login", (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, "../build/bundle.html"));
-});
-
-app.get("/home", (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, "../build/bundle.html"));
-});
-
-// Going to take query strings
-app.get("/board", (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, "../build/bundle.html"));
-});
-//stretch feature
-// app.get("/teams", (req, res) => {
+// app.get("/home", (req, res) => {
 //   return res.status(200).sendFile(path.join(__dirname, "../build/bundle.html"));
 // });
 
 // app.use("/build", express.static(path.join(__dirname, "../build")));
 
-// app.use((req, res) =>
-//   res.status(404).send("Sorry! The page you're looking for doesn't exist")
-// );
+app.use((req, res) =>
+  res.status(404).redirect('/')
+);
 
 app.use((err, req, res, next) => {
   const defaultErr = {
@@ -185,8 +52,8 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(BACKEND_PORT, () => {
+  console.log(`Listening on port ${BACKEND_PORT}`);
 });
 
 module.exports = app;
---
+++ server/routes/api.js
@@ -0,0 +1,156 @@
+const express = require("express");
+const boardsController = require("../controllers/boardsController");
+const cookieController = require("../controllers/cookieController");
+const userController = require("../controllers/userController");
+
+const router = express.Router();
+
+router.post(
+  "/signup",
+  userController.validateUsername,
+  userController.createUser,
+  cookieController.setSSIDCookie,
+  (req, res) => {
+    if (res.locals.createdUser === true) {
+      res.status(200).json({
+        username: res.locals.username,
+        boards: [],
+      });
+    } else {
+      res.status(501).send("Error Signing up");
+    }
+  }
+);
+
+router.get("/login", cookieController.validateSSID, (req, res) => {
+  if (res.locals.ssidIsValid) res.status(200).json(res.locals.boardInfo);
+  else res.status(501).sendStatus("Invalid SSID");
+});
+
+router.post(
+  "/login",
+  userController.validateUsername,
+  userController.validatePassword,
+  cookieController.setSSIDCookie,
+  (req, res) => {
+    if (res.locals.passwordIsValid && res.locals.usernameIsValid) {
+      res.status(200).json({
+        username: res.locals.username,
+        boards: res.locals.boards,
+      });
+    } else {
+      res.status(501).send("Error Logging In, Wrong Username or Password");
+    }
+  }
+);
+
+router.get(
+  "/dashboard",
+  cookieController.validateSSID,
+  cookieController.blockInvalidSession,
+  userController.getAllBoardsFromUser,
+  (req, res) => {
+    if (res.locals.ssidIsValid) res.status(200).json(res.locals.boardInfo);
+    else res.status(501).sendStatus("Invalid SSID");
+  }
+);
+
+router.post(
+  "/board",
+  cookieController.validateSSID,
+  cookieController.blockInvalidSession,
+  boardsController.createBoard,
+  // userController.getAllBoardsFromUser,
+  (req, res) => {
+    return res.status(200).json(res.locals.createdBoard);
+  }
+);
+
+router.delete(
+  "/board",
+  cookieController.validateSSID,
+  cookieController.blockInvalidSession,
+  boardsController.deleteBoard,
+  (req, res) => {
+    if (res.locals.ssidIsValid) res.sendStatus(200);
+    else res.status(501).sendStatus("Invalid SSID");
+  }
+);
+
+router.get(
+  "/board/:id",
+  cookieController.validateSSID,
+  cookieController.blockInvalidSession,
+  boardsController.getBoardFromUser,
+  (req, res) => {
+    return res.status(200).json(res.locals.boardInfo);
+  }
+);
+
+router.post(
+  "/story",
+  cookieController.validateSSID,
+  cookieController.blockInvalidSession,
+  boardsController.createStory,
+  boardsController.getBoardFromUserUsingCache,
+  (req, res) => {
+    return res.status(200).json(res.locals.boardInfo);
+  }
+);
+
+router.post(
+  "/task",
+  cookieController.validateSSID,
+  cookieController.blockInvalidSession,
+  boardsController.createTask,
+  boardsController.getBoardFromUserUsingCache,
+  (req, res) => {
+    return res.status(200).json(res.locals.boardInfo);
+  }
+);
+
+router.delete(
+  "/board",
+  cookieController.validateSSID,
+  cookieController.blockInvalidSession,
+  boardsController.deleteBoard,
+  userController.getAllBoardsFromUser,
+  (req, res) => {
+    return res.status(200).json(res.locals.boardInfo);
+  }
+);
+
+router.delete(
+  "/story",
+  cookieController.validateSSID,
+  cookieController.blockInvalidSession,
+  boardsController.deleteStory,
+  boardsController.getBoardFromUserUsingCache,
+  (req, res) => {
+    return res.status(200).json(res.locals.boardInfo);
+  }
+);
+
+router.delete(
+  "/task",
+  cookieController.validateSSID,
+  cookieController.blockInvalidSession,
+  boardsController.deleteTask,
+  boardsController.getBoardFromUserUsingCache,
+  (req, res) => {
+    return res.status(200).json(res.locals.boardInfo);
+  }
+);
+
+//stretch feature
+// app.get("/teams", (req, res) => {
+//   return res.status(200).sendFile(path.join(__dirname, "../build/bundle.html"));
+// });
+
+// app.use("/build", express.static(path.join(__dirname, "../build")));
+
+// app.use((req, res) =>
+//   res.status(404).send("Sorry! The page you're looking for doesn't exist")
+// );
+
+module.exports = router;
